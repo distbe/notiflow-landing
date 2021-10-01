e929929b08a727a45d2a5808dd65af92c2a56e8e
Changwan Jun <wan2land@gmail.com>
2021-10-02 03:42:43 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div class="container mx-auto">
-    <div class="flex flex-col-reverse">
-      <template v-for="(message, messageIndex) in messages" :key="messageIndex">
-        <div class="whitespace-pre">[POST] {{ message }}</div>
+  <div class="container mx-auto max-w-5xl p-4">
+    <div class="flex flex-col-reverse font-mono gap-y-3">
+      <template v-for="(log, logIndex) in logs" :key="logIndex">
+        <div>
+          <div class="text-sm text-gray-400">{{ format(log.created.seconds * 1000, 'yyyy-MM-dd HH:mm:ss') }}</div>
+          <div class="text-sm whitespace-pre-line break-all">{{ JSON.stringify(log.body, null, '  ').replace(/ /g, '&nbsp;') }}</div>
+        </div>
       </template>
     </div>
   </div>
 </template>
 <script setup lang="ts">
 import { onUnmounted, ref } from 'vue'
+import format from 'date-fns/format'
 import { onSnapshot } from "firebase/firestore";
-import { collection, query, where } from "firebase/firestore";
+import { collection, query, where, Timestamp } from "firebase/firestore";
 import { db } from "./modules/firebase"
 
-const messages = ref([] as string[])
+const logs = ref([] as { body: string, created: { seconds: number } }[])
 
 const { searchParams } = new URL(location.href)
 const channel = searchParams.get('channel')
 
 if (channel) {
-  const q = query(collection(db, "logs"), where("channel", "==", channel));
+  const date = new Date()
+  const q = query(
+    collection(db, `channels/${channel}/logs`),
+    where("created", ">=", Timestamp.fromDate(date))
+  );
   const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    const datas = [] as string[];
-    querySnapshot.forEach((doc) => {
-      datas.push(doc.data().body);
+    querySnapshot.docChanges().forEach((change) => {
+      if (change.type === 'added') {
+        const data = change.doc.data()
+        logs.value.push({
+          body: JSON.parse(data.body),
+          created: data.created,
+        });
+      }
     });
-    console.log(datas)
-    messages.value = datas
   });
 
   onUnmounted(() => {
